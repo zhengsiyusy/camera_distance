cde2d5076a914ff26a384a281927a0870b8cde3e
zhengsiyu <1241735127@qq.com>
2022-12-05 16:03:40 +0800

    增加日志功能，优化界面显示，增加按钮提示功能

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # / -*-codeing = utf-8  -*-
 # TIME : 2022/11/22 10:16
 # File : my_camera_mainwindow
+import os
 import sys
 import cv2
 import math
@@ -8,8 +9,9 @@ import numpy as np
 from PyQt5.QtCore import QCoreApplication, Qt
 from PyQt5.QtWidgets import QMainWindow, QLabel, QWidget, QFileDialog, QApplication
 from PyQt5 import QtCore, QtWidgets, QtGui
-from PyQt5.QtGui import QGuiApplication
 from camera import Ui_camera
+from loginfo.log import init_log
+from PyQt5.QtMultimedia import QCameraInfo
 
 
 class QmyMainWindow(QMainWindow):
@@ -22,6 +24,32 @@ class QmyMainWindow(QMainWindow):
         self.CAM_NUM = 0  # 为0时表示视频流来自笔记本内置摄像头
         self.slot_init()  # 初始化槽函数
         self.setWindowTitle("参数校准工具")
+        self.button_status()
+        self.U = 0
+        self.V = 0
+        name = str("像素坐标：U:%.3f, V:%.3f " % (self.U, self.V))
+        self.LabImagePOS = QLabel(name)
+        self.ui.statusbar.addWidget(self.LabImagePOS)
+        # 日志管理
+        current_path = os.getcwd()
+        path = os.path.join(current_path, 'camera.log')
+        self.log = init_log(path)
+        self.camera = None  # QCamera对象
+        self.get_camera_info()
+
+    def get_camera_info(self):
+        cameras = QCameraInfo.availableCameras()  # list[QCameraInfo]
+        print(str(len(cameras)) + " camera find!")
+        if len(cameras) > 0:
+            camInfo = QCameraInfo.defaultCamera()
+            info = camInfo.description()
+            self.log.info("The camera image parameters are as follows:\n %s" % info)
+            self.log.info("Camera preview image resolution: 320*240")
+            self.log.info("Image resolution of shooting picture: 640*480")
+        else:
+            self.log.warning("The camera doesn't exist.")
+
+    def button_status(self):
         # 设置默认选中计算距离模式
         self.ui.calculate_distance.setChecked(True)
         # 默认距离计算框不可编辑
@@ -30,17 +58,61 @@ class QmyMainWindow(QMainWindow):
         self.ui.show_image.setMouseTracking(True)
         self.ui.centralwidget.setMouseTracking(True)
         self.setMouseTracking(True)
-        self.U = 0
-        self.V = 0
-        name = str("像素坐标：U:%.3f, V:%.3f " % (self.U, self.V))
-        self.__LabImagePOS = QLabel(name)
-        self.ui.statusbar.addWidget(self.__LabImagePOS)
+
+    def open_camera(self):
+        if not self.timer_camera.isActive():  # 若定时器未启动
+            flag = self.cap.open(self.CAM_NUM)  # 参数是0，表示打开笔记本的内置摄像头，参数是视频文件路径则打开视频
+            if not flag:  # flag表示open()成不成功
+                msg = QtWidgets.QMessageBox.warning(self, '警告', "请检查相机与电脑是否连接正确", buttons=QtWidgets.QMessageBox.Ok)
+                self.log.warning("Please check that the camera is properly connected to the computer")
+            else:
+                self.timer_camera.start(30)  # 定时器开始计时30ms，结果是每过30ms从摄像头中取一帧显示
+                self.ui.open_cam.setText("关闭相机")
+        else:
+            self.timer_camera.stop()  # 关闭定时器
+            self.cap.release()  # 释放视频流
+            self.ui.frame1.clear()  # 清空视频显示区域
+            self.ui.open_cam.setText("打开相机")
+
+    def save_parameter(self):
+        if not self.timer_camera.isActive():  # 若定时器未启动
+            msg = QtWidgets.QMessageBox.warning(self, '警告', "请先打开相机", buttons=QtWidgets.QMessageBox.Ok)
+        else:
+            self.timer_camera.stop()  # 关闭定时器
+            self.cap.release()  # 释放视频流
+            self.ui.frame1.clear()  # 清空视频显示区域
+
+    def get_image(self):
+        if not self.timer_camera.isActive():  # 若定时器未启动
+            msg = QtWidgets.QMessageBox.warning(self, '警告', "请先打开相机", buttons=QtWidgets.QMessageBox.Ok)
+        else:
+            QmyMainWindow.show_camera(self, "frame2")
+
+    def show_camera(self, port="frame1"):
+        flag, self.image = self.cap.read()  # 从视频流中读取
+        if not flag:
+            self.log.warning("read image failed.")
+        if port == "frame1":
+            # 把读到的帧的大小设置为 320x240
+            show = cv2.resize(self.image, (320, 240))
+            # 视频色彩转换回RGB，这样才是现实的颜色
+            show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)
+            # 把读取到的视频数据变成QImage形式
+            showImage = QtGui.QImage(show.data, show.shape[1], show.shape[0], QtGui.QImage.Format_RGB888)
+            self.ui.frame1.setPixmap(QtGui.QPixmap.fromImage(showImage))  # 往显示视频的Label里 显示QImage
+        else:
+            # 把读到的帧的大小重新设置为 640x480
+            show = cv2.resize(self.image, (640, 480))
+            # 视频色彩转换回RGB，这样才是现实的颜色
+            show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)
+            showImage = QtGui.QImage(show.data, show.shape[1], show.shape[0], QtGui.QImage.Format_RGB888)
+            self.ui.frame2.setPixmap(QtGui.QPixmap.fromImage(showImage))  # 往显示视频的Label里 显示QImage
 
     '''初始化所有槽函数'''
     def slot_init(self):
         self.timer_camera.timeout.connect(self.show_camera)  # 若定时器结束，则调用show_camera()
         self.ui.open_cam.clicked.connect(self.open_camera)
-        self.ui.close_cam.clicked.connect(self.close_camera)
+        # self.ui.save_para.clicked.connect(self.save_parameter)
         self.ui.get_image.clicked.connect(self.get_image)
         self.ui.calculate_camera.clicked.connect(self.calculate_camera_result)
         self.ui.calculate_position.clicked.connect(self.calculate_position_result)
@@ -57,7 +129,7 @@ class QmyMainWindow(QMainWindow):
         snapPos = QtCore.QPointF(snapPosX, snapPosY)
         self.U = snapPos.x() - 658
         self.V = snapPos.y() - 271
-        self.__LabImagePOS.setText("像素坐标：U:%.3f, V:%.3f " % (self.U, self.V - 20))
+        self.LabImagePOS.setText("像素坐标：U:%.3f, V:%.3f " % (self.U, self.V - 20))
 
     def mode_select(self):
         if self.ui.calculate_distance.isChecked():
@@ -87,6 +159,7 @@ class QmyMainWindow(QMainWindow):
             angle_value = self.ui.camera_angle.value()
             if height_value == 0 or angle_value == 0:
                 msg = QtWidgets.QMessageBox.warning(self, '警告', "请输入正确的参数", buttons=QtWidgets.QMessageBox.Ok)
+                self.log.warning("calculate distance:input data incorrect")
             else:
                 # 设置距离计算框不可编辑
                 self.ui.camera_farthest.setEnabled(False)
@@ -99,8 +172,9 @@ class QmyMainWindow(QMainWindow):
             # read camera config
             height_value = self.ui.camera_height.value()
             farthest_value = self.ui.camera_farthest.value()
-            if height_value == 0 or farthest_value == 0:  # or farthest_value == 0:
+            if height_value == 0 or farthest_value == 0:
                 msg = QtWidgets.QMessageBox.warning(self, '警告', "请输入正确的参数", buttons=QtWidgets.QMessageBox.Ok)
+                self.log.warning("camera angle:input data incorrect")
             else:
                 QmyMainWindow.cal_angle(self, height_value, farthest_value)
 
@@ -111,6 +185,10 @@ class QmyMainWindow(QMainWindow):
         farthest = height_value * math.tan(radians_value)
         # 保留2位小数
         final_farthest = round(farthest, 2)
+        self.log.info("Calculate the furthest distance measured by the camera.")
+        self.log.info("height: %s cm" % height_value)
+        self.log.info("angle: %s °" % angle_value)
+        self.log.info("distance: %s cm" % final_farthest)
         # 显示到UI界面
         self.ui.camera_farthest.setValue(final_farthest)
 
@@ -121,6 +199,10 @@ class QmyMainWindow(QMainWindow):
         final_angle = math.degrees(angle_value)
         # 保留2位小数
         final_angle = round(final_angle, 2)
+        self.log.info("Calculate camera mounting Angle.")
+        self.log.info("height: %s cm" % height_value)
+        self.log.info("distance: %s cm" % farthest_value)
+        self.log.info("angle: %s °" % final_angle)
         self.ui.camera_angle.setValue(final_angle)
 
     def calculate_position_result(self):
@@ -174,11 +256,15 @@ class QmyMainWindow(QMainWindow):
                 (px_u2_value == 0 and px_v2_value == 0 and cm_x2_value == 0 and cm_y2_value == 0) or
                 (px_u3_value == 0 and px_v3_value == 0 and cm_x3_value == 0 and cm_y3_value == 0) or
                 (px_u4_value == 0 and px_v4_value == 0 and cm_x1_value == 0 and cm_y4_value == 0)):
-            msg = QtWidgets.QMessageBox.warning(self, '警告', "坐标出入参数有误，请重新输入", buttons=QtWidgets.QMessageBox.Ok)
+            msg = QtWidgets.QMessageBox.warning(self, '警告', "输入坐标参数有误，请重新输入", buttons=QtWidgets.QMessageBox.Ok)
         matrix, status = cv2.findHomography(pts_src, pts_dst, method=cv2.RANSAC, ransacReprojThreshold=1)
         print("转换矩阵", matrix)
         if matrix is not None:
             msg = QtWidgets.QMessageBox.information(self, '校准', "校准成功", buttons=QtWidgets.QMessageBox.Ok)
+            self.log.info("Coordinate of pixels:\n %s" % pts_src)
+            self.log.info("Actual coordinates:\n %s" % pts_dst)
+            self.log.info("Matrix of transformation:\n %s" % matrix)
+            self.log.info("position: Parameter calibration succeeded!")
             with open('matrix_config.cfg', 'w') as f_cfg:
                 # 发送转换矩阵方式
                 # for i in range(3):
@@ -219,46 +305,8 @@ class QmyMainWindow(QMainWindow):
                 f_cfg.write('\n')
                 f_cfg.write(str(cm_y4_value))
                 f_cfg.write('\n')
-
-    def open_camera(self):
-        if not self.timer_camera.isActive():  # 若定时器未启动
-            flag = self.cap.open(self.CAM_NUM)  # 参数是0，表示打开笔记本的内置摄像头，参数是视频文件路径则打开视频
-            if not flag:  # flag表示open()成不成功
-                msg = QtWidgets.QMessageBox.warning(self, '警告', "请检查相机与电脑是否连接正确", buttons=QtWidgets.QMessageBox.Ok)
-            else:
-                self.timer_camera.start(30)  # 定时器开始计时30ms，结果是每过30ms从摄像头中取一帧显示
-
-    def close_camera(self):
-        if not self.timer_camera.isActive():  # 若定时器未启动
-            msg = QtWidgets.QMessageBox.warning(self, '警告', "请先打开相机", buttons=QtWidgets.QMessageBox.Ok)
         else:
-            self.timer_camera.stop()  # 关闭定时器
-            self.cap.release()  # 释放视频流
-            self.ui.frame1.clear()  # 清空视频显示区域
-
-    def get_image(self):
-        if not self.timer_camera.isActive():  # 若定时器未启动
-            msg = QtWidgets.QMessageBox.warning(self, '警告', "请先打开相机", buttons=QtWidgets.QMessageBox.Ok)
-        else:
-            QmyMainWindow.show_camera(self, "frame2")
-
-    def show_camera(self, port="frame1"):
-        flag, self.image = self.cap.read()  # 从视频流中读取
-        if port == "frame1":
-            # 把读到的帧的大小设置为 320x240
-            show = cv2.resize(self.image, (320, 240))
-            # 视频色彩转换回RGB，这样才是现实的颜色
-            show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)
-            # 把读取到的视频数据变成QImage形式
-            showImage = QtGui.QImage(show.data, show.shape[1], show.shape[0], QtGui.QImage.Format_RGB888)
-            self.ui.frame1.setPixmap(QtGui.QPixmap.fromImage(showImage))  # 往显示视频的Label里 显示QImage
-        else:
-            # 把读到的帧的大小重新设置为 640x480
-            show = cv2.resize(self.image, (640, 480))
-            # 视频色彩转换回RGB，这样才是现实的颜色
-            show = cv2.cvtColor(show, cv2.COLOR_BGR2RGB)
-            showImage = QtGui.QImage(show.data, show.shape[1], show.shape[0], QtGui.QImage.Format_RGB888)
-            self.ui.frame2.setPixmap(QtGui.QPixmap.fromImage(showImage))  # 往显示视频的Label里 显示QImage
+            self.log.error("position: Failure of calibration")
 
 
 if __name__ == '__main__':
